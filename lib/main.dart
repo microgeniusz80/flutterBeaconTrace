ea9055ef365fd79a301e5c54ebf99938d2d4a561
microgeniusz80 <ilyas.ukm@gmail.com>
2023-09-04 16:28:19 +0800

    cleaned. receiver complete.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'dart:async';
-import 'dart:io';
-import 'dart:ui';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_background_service/flutter_background_service.dart';
-import 'package:flutter_background_service_android/flutter_background_service_android.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:bluetooth_enable_fork/bluetooth_enable_fork.dart';
 import 'package:flutter/services.dart';
@@ -40,7 +34,6 @@ Future<void> main() async {
     BluetoothEnable.enableBluetooth.then((result) async {
       if (result == "true") {
         await startScanUUID();
-        await initializeService();
         runApp(const MyApp());
       } else if (result == "false") {
         print('Bluetooth not enabled');
@@ -55,7 +48,6 @@ Future<void> main() async {
   
 }
 
-//check initstate initplatformstate supported or not later, line58 bleedit
 Future<void> startScanUUID() async {
 
   SharedPreferences preferences = await SharedPreferences.getInstance();
@@ -114,150 +106,6 @@ Future<void> startScanUUID() async {
   await _scanDevices();
 }
 
-Future<void> initializeService() async {
-  final service = FlutterBackgroundService();
-
-  /// OPTIONAL, using custom notification channel id
-  const AndroidNotificationChannel channel = AndroidNotificationChannel (
-    'my_foreground', // id
-    'MY FOREGROUND SERVICE', // title
-    description: 'This channel is used for important notifications.', // description
-    importance: Importance.low, // importance must be at low or higher level
-  );
-
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
-
-  if (Platform.isIOS) {
-    await flutterLocalNotificationsPlugin.initialize(
-      const InitializationSettings(
-        iOS: IOSInitializationSettings(),
-      ),
-    );
-  }
-
-  await flutterLocalNotificationsPlugin
-      .resolvePlatformSpecificImplementation<
-          AndroidFlutterLocalNotificationsPlugin>()
-      ?.createNotificationChannel(channel);
-
-  await service.configure(
-    androidConfiguration: AndroidConfiguration(
-      // this will be executed when app is in foreground or background in separated isolate
-      onStart: onStart,
-
-      // auto start service
-      autoStart: true,
-      isForegroundMode: true,
-
-      notificationChannelId: 'my_foreground',
-      initialNotificationTitle: 'Contact Tracing',
-      initialNotificationContent: 'Initializing',
-      foregroundServiceNotificationId: 888,
-    ),
-    iosConfiguration: IosConfiguration(
-      // auto start service
-      autoStart: true,
-
-      // this will be executed when app is in foreground in separated isolate
-      onForeground: onStart,
-
-      // you have to enable background fetch capability on xcode project
-      onBackground: onIosBackground,
-    ),
-  );
-
-  service.startService();
-}
-
-@pragma('vm:entry-point')
-Future<bool> onIosBackground(ServiceInstance service) async {
-  WidgetsFlutterBinding.ensureInitialized();
-  DartPluginRegistrant.ensureInitialized();
-
-  print('iOS - im in background now');
-
-  SharedPreferences preferences = await SharedPreferences.getInstance();
-  await preferences.reload();
-  final log = preferences.getStringList('log') ?? <String>[];
-  log.add(DateTime.now().toIso8601String());
-  await preferences.setStringList('log', log);
-
-  return true;
-}
-
-@pragma('vm:entry-point')
-void onStart(ServiceInstance service) async {
-  // Only available for flutter 3.0.0 and later
-  DartPluginRegistrant.ensureInitialized();
-
-  /// OPTIONAL when use custom notification
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
-
-  if (service is AndroidServiceInstance) {
-    service.on('setAsForeground').listen((event) {
-      service.setAsForegroundService();
-    });
-
-    service.on('setAsBackground').listen((event) {
-      service.setAsBackgroundService();
-    });
-  }
-
-  service.on('stopService').listen((event) {
-    service.stopSelf();
-  });
-
-
-  // bring to foreground
-  Timer.periodic(const Duration(seconds: 10), (timer) async {
-
-    if (service is AndroidServiceInstance) {
-      if (await service.isForegroundService()) {
-        print('timer firing every 10 sec');
-      }
-    }
-
-    /// the notification id must be equals with AndroidConfiguration when you call configure() method.
-    flutterLocalNotificationsPlugin.show (
-      888,
-      'Contact Tracing',
-      'Time: ${DateTime.now()}',
-      const NotificationDetails(
-        android: AndroidNotificationDetails(
-          'my_foreground',
-          'MY FOREGROUND SERVICE',
-          icon: 'ic_bg_service_small',
-          ongoing: true,
-        ),
-      ),
-    );
-
-  });
-
-  // test using external plugin
-  final deviceInfo = DeviceInfoPlugin();
-  String? device;
-  if (Platform.isAndroid) {
-    final androidInfo = await deviceInfo.androidInfo;
-    device = androidInfo.model;
-  }
-
-  if (Platform.isIOS) {
-    final iosInfo = await deviceInfo.iosInfo;
-    device = iosInfo.model;
-  }
-
-  service.invoke(
-    'update',
-    {
-      "current_date": DateTime.now().toIso8601String(),
-      "device": device,
-    },
-  );
-}
-
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -267,7 +115,6 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   
-  String text = "Stop Service";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -277,38 +124,6 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            StreamBuilder<Map<String, dynamic>?>(
-              stream: FlutterBackgroundService().on('update'),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-
-                final data = snapshot.data!;
-                String? device = data["device"];
-                DateTime? date = DateTime.tryParse(data["current_date"]);
-                return Column(
-                  children: [
-                    Text(device ?? 'Unknown'),
-                    Text(date.toString()),
-                  ],
-                );
-              },
-            ),
-            ElevatedButton(
-              child: const Text("Foreground Mode"),
-              onPressed: () {
-                FlutterBackgroundService().invoke("setAsForeground");
-              },
-            ),
-            ElevatedButton(
-              child: const Text("Background Mode"),
-              onPressed: () {
-                FlutterBackgroundService().invoke("setAsBackground");
-              },
-            ),
             ElevatedButton(
               child: const Text("show contents"),
               onPressed: () async {
@@ -317,30 +132,7 @@ class _MyAppState extends State<MyApp> {
                 print ('kandungan sharedPref: ${sp.getString('nama')}');
               },
             ),
-            ElevatedButton(
-              child: Text(text),
-              onPressed: () async {
-                final service = FlutterBackgroundService();
-                var isRunning = await service.isRunning();
-                if (isRunning) {
-                  service.invoke("stopService");
-                } else {
-                  service.startService();
-                }
-
-                if (!isRunning) {
-                  text = 'Stop Service';
-                } else {
-                  text = 'Start Service';
-                }
-                setState(() {});
-              },
-            )
           ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.play_arrow),
         ),
       ),
     );
